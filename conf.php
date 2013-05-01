9eaa5b23dc6cb674b73b3a630df1862a1ba533bd
Bernhard Rieder <berno.rieder@gmail.com>
2013-05-01 14:57:23 +0200

    next

---
+++ conf.php
@@ -0,0 +1,9 @@
+<?php
+
+// these variables need to be adapted
+// API key can be obtained here: http://developer.nytimes.com/page
+
+$term = 'yourquery';							// use '"global warming"' for phrases
+$key_search = "your_NYY_search_api_key";
+
+?>
