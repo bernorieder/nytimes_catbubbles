9d2cb2f32fb51413ad60cda56ea41b74c6d8bc7d
Bernhard Rieder <rieder@mymba.lan>
2013-06-26 19:10:30 +0200

    test

--- analyze.php
+++ analyze.php
@@ -8,6 +8,7 @@
 * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
 */
 
+
 // adapt to desired value
 $minyears = 10;		// specifies the number of year a category has to appear in order to be included
 
